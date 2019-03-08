32636471b3289afc2fcdc5492e05219fab376828
dirtshell <jake@jacobkenin.com>
2019-03-08 00:12:57 -0500

    Now cors anywhere works, and we can access the API.

--- main.js
+++ main.js
@@ -29,9 +29,25 @@ liveServer.start(liveServerParams);
 
 
 // ============================================================================
+// Setup a reverse proxy to avoid CORS issues
+// ============================================================================
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
+
+// ============================================================================
 // Electron code
 // ============================================================================
-
 /*
  * Creates the browser window process
  */
