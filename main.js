bfd7c399bd784d6d234605ead665c907eb232996
dirtshell <jake@jacobkenin.com>
2019-03-07 23:47:47 -0500

    Issues with CORS messing up our headers... Gonna try again using a different cors proxy.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const url = require('url')
 const nodeConsole = require('console')
 const { app, BrowserWindow } = require('electron')
 
@@ -7,6 +8,33 @@ const { app, BrowserWindow } = require('electron')
 let win
 let myConsole = new nodeConsole.Console(process.stdout, process.stderr);
 
+// ============================================================================
+// Start a liveserver that hosts all of our assets so Aframe works
+// ============================================================================
+const liveServer = require("live-server")
+let myPath = path.resolve(__dirname, '.');
+const liveServerParams = {
+    port: 8181, // Set the server port. Defaults to 8080.
+    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+    root: myPath, // Set root directory that's being served. Defaults to cwd.
+    open: false, // When false, it won't load your browser by default.
+    ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+    file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+    wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+    mount: [['/components', './node_modules']], // Mount a directory to a route.
+    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+    middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+liveServer.start(liveServerParams);
+
+
+// ============================================================================
+// Electron code
+// ============================================================================
+
+/*
+ * Creates the browser window process
+ */
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({ width: 1080, height: 900 })
@@ -14,8 +42,15 @@ function createWindow () {
   // Run as a single instance app
   makeSingleInstance()
 
+  // Enable dev tools
+  win.webContents.openDevTools()
+
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadURL(url.format({
+    pathname: "localhost:8181",
+    protocol: 'http:',
+    slashes: true
+  }))
 
   // Emitted when the window is closed.
   win.on('closed', () => {
